fa25be9f06d23126350731d469437b78915bf640
Krish Patel <krishpkreame1405+github@gmail.com>
2024-02-25 01:50:31 +1300

    Add support for passing credentials to RedditAPI constructor
    - Added credentials file so env vars are not needed and can be run by anyone
    - Added better user input
    - Refactored part of the code and added better documentation

--- main.py
+++ main.py
@@ -2,34 +2,109 @@ from reddit import RedditAPI
 from tiktokvoice import tts, get_duration, merge_audio_files
 from srt import gen_srt_file
 from editor import VideoEditor
-reddit = RedditAPI("AmItheAsshole")
-
-# x = reddit.get_from_url("""
-# https://www.reddit.com/r/AmItheAsshole/comments/11e01qq/aita_for_not_giving_my_sister_her_wedding_dress/
-# """)
-
-x = reddit.get_top_posts(10)[8]
+import subprocess
+import os
 
 
-print("Title :", x["title"])
-print("Subred :", x["subreddit"])
-print("Time :", x["time"])
-print("ID :", x["id"])
-print("No. of lines :", len(x["content"]))
+def setup_credentials():
+    print("\n"*10, "-"*30, "\nSetting up the credentials\n")
+    print("You will need to create a Reddit app and get the client id and client secret\n")
+    print("You will also need to enter your Reddit username and password\n")
+    print("These credentials will be stored in a file called credentials.txt\n")
+    print("This file will be stored locally and will only be used to authenticate with Reddit and to get the post text\n")
+    print("Goto https://github.com/Krishpkreame/RSCG and follow the instructions\n")
+    print("I would recommend creating a new throwaway Reddit account\n")
+    print("This will only run the first time you run the program\n")
+    client_id = input("Enter the client id:\n")
+    client_secret = input("Enter the client secret:\n")
+    username = input("Enter the username:\n")
+    password = input("Enter the password:\n")
+    with open("credentials.txt", "w") as f:
+        f.write(f"{client_id}\n{client_secret}\n{username}\n{password}")
 
-script = []
-content = [x["title"]] + x["content"]
-for item, i in zip(content, range(0, len(content))):
-    filename = f"outputs/temp_{x['id']}_{i}.mp3"
-    tts(item, "en_us_001", filename, 1.15)
-    dur = get_duration(filename)
-    script.append((item, dur))
-print("Created audio files for script")
-srt_path = f"inputs/{x['id']}.srt"
-gen_srt_file(script, srt_path, 0.1)
-wav_path = f"inputs/{x['id']}.wav"
-totaldur = merge_audio_files(wav_path, 0.1)
-print("Merged audio duration:", totaldur, "seconds")
 
-v = VideoEditor(totaldur, srt_path, wav_path, False)
-v.start_render(f"outputs/{x['id']}.mp4")
+if __name__ == "__main__":
+    # Check if ffmpeg is installed
+    try:
+        subprocess.run(["ffmpeg", "-version"], capture_output=True, check=True)
+    except subprocess.CalledProcessError:
+        print("ffmpeg is not installed. Please install ffmpeg to continue.")
+        exit(1)
+
+    # Check if the credentials file exists
+    if not os.path.exists("credentials.txt"):
+        print("No credentials file found, going through the setup - ")
+        setup_credentials()
+
+    # Check if the outputs folder exists
+    if not os.path.exists("outputs"):
+        os.mkdir("outputs")
+        print("Created inputs folder\n - This is where final videos will be saved.")
+    # Check if the inputs folder exists
+    if not os.path.exists("inputs"):
+        os.mkdir("inputs")
+        print("Created inputs folder\n - This is where input video files will need to be stored\n - The srt and wav files will also be stored here.")
+
+    # Check if any mp4 files are present in the inputs folder
+    if len([i for i in os.listdir("inputs") if i.endswith(".mp4")]) == 0:
+        print("No input video files found in the inputs folder, Add your input video files to this folder")
+        exit(1)
+
+    # Read the credentials from the file
+    with open("credentials.txt", "r") as f:
+        creds = f.readlines()
+
+    # Try create the Reddit instance with the credentials.
+    try:
+        reddit = RedditAPI(
+            creds[0].strip(),  # client_id
+            creds[1].strip(),  # client_secret
+            creds[2].strip(),  # username
+            creds[3].strip())  # password
+    except:
+        print("credentials not set correctly, delete credentials.txt and setup again")
+        exit(1)
+
+    print("\n"*10, "-"*30, "\nWelcome to the Reddit to TikTok Video Creator\n")
+
+    url = input("Enter the Reddit post URL:\n")
+
+    # Get the post from the URL
+    post = reddit.get_from_url(url)
+
+    # Print the post details
+    print("Title :", post["title"])
+    print("Subred :", post["subreddit"])
+    print("Time :", post["time"])
+    print("ID :", post["id"])
+    print("No. of lines :", len(post["content"]))
+
+    # Ask if the user wants to proceed
+    proceed = input("Do you want to proceed? (y/n)\n")
+    if proceed.lower() != "y":
+        print("Exiting")
+        exit(0)
+
+    # Create the audio files for each sentence using the script
+    script = []
+    content = [post["title"]] + post["content"]
+
+    for item, i in zip(content, range(0, len(content))):
+        filename = f"outputs/temp_{post['id']}_{i}.mp3"
+        tts(item, "en_us_001", filename, 1.15)
+        dur = get_duration(filename)
+        script.append((item, dur))
+    print("Created audio files for script")
+
+    # Create the srt using the script
+    srt_path = f"inputs/{post['id']}.srt"
+    gen_srt_file(script, srt_path, 0.1)
+
+    # Merge the audio files into one
+    wav_path = f"inputs/{post['id']}.wav"
+    totaldur = merge_audio_files(wav_path, 0.1)
+    print("Merged audio duration:", totaldur, "seconds")
+
+    # Create the video
+    v = VideoEditor(totaldur, srt_path, wav_path, False)
+    v.start_render(f"outputs/{post['id']}.mp4")
--- reddit.py
+++ reddit.py
@@ -1,43 +1,45 @@
 from datetime import datetime
 import praw
-import os
 from ftfy import ftfy
 
 
 class RedditAPI:
-    def __init__(self, subreddit: str):
+    def __init__(self, client_id: str = None, client_secret: str = None, username: str = None, password: str = None):
         """
-        Initializes the RedditAPI class.
+        Initializes a Reddit object with the provided credentials.
 
-        Parameters:
-        - subreddit (str): The name of the subreddit.
+        Args:
+            client_id (str): The client ID for the Reddit API.
+            client_secret (str): The client secret for the Reddit API.
+            username (str): The username for the Reddit account.
+            password (str): The password for the Reddit account.
 
         Raises:
-        - ValueError: If any of the required environment variables are not set.
+            ValueError: If any of the required credentials are not provided.
         """
         # Raise error if the environment variables are not set
-        if not os.environ['REDDIT_CLIENT_ID']:
-            raise ValueError("REDDIT_CLIENT_ID env var not set")
-        if not os.environ['REDDIT_CLIENT_SECRET']:
-            raise ValueError("REDDIT_CLIENT_SECRET env var not set")
-        if not os.environ['REDDIT_USERNAME']:
-            raise ValueError("REDDIT_USERNAME env var not set")
-        if not os.environ['REDDIT_PASSWORD']:
-            raise ValueError("REDDIT_PASSWORD env var not set")
-        if not subreddit:
-            raise ValueError("Subreddit not set")
+        if not client_id:
+            raise ValueError(
+                "REDDIT_CLIENT_ID not set correctly, delete credentials.txt and setup again")
+        if not client_secret:
+            raise ValueError(
+                "REDDIT_CLIENT_SECRET not set correctly, delete credentials.txt and setup again")
+        if not username:
+            raise ValueError(
+                "REDDIT_USERNAME not set correctly, delete credentials.txt and setup again")
+        if not password:
+            raise ValueError(
+                "REDDIT_PASSWORD not set correctly, delete credentials.txt and setup again")
 
         # Create the Reddit instance
         self.reddit = praw.Reddit(
-            client_id=os.environ['REDDIT_CLIENT_ID'],
-            client_secret=os.environ['REDDIT_CLIENT_SECRET'],
-            user_agent='RSCG (by u/RedditThrowaway4723)',
-            username=os.environ['REDDIT_USERNAME'],
-            password=os.environ['REDDIT_PASSWORD'])
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent='RSCG',
+            username=username,
+            password=password)
         # Set the config to decode HTML entities
         self.reddit.config.decode_html_entities = True
-        # Set the subreddit
-        self.subreddit = self.reddit.subreddit(subreddit)
 
     def __utc_to_datetimestr(self, utc: float):
         """
@@ -102,22 +104,26 @@ class RedditAPI:
             "content": self.__filter_content(self.post.selftext)
         }
 
-    def get_top_posts(self, limit: int = 10):
+    def get_top_posts(self, subreddit: str, limit: int = 10):
         """
-        Retrieves information about the top posts from the subreddit.
+        Retrieves the top posts from a specified subreddit.
 
-        Parameters:
-        - limit (int): The maximum number of posts to retrieve. Default is 10.
+        Args:
+            subreddit (str): The name of the subreddit.
+            limit (int, optional): The maximum number of posts to retrieve. Defaults to 10.
 
         Returns:
-        - list: A list of dictionaries containing information about the top posts.
+            list: A list of dictionaries containing information about each top post.
                 Each dictionary contains the following keys:
-                - subreddit (str): The name of the subreddit.
-                - id (str): The ID of the post.
-                - title (str): The title of the post.
-                - time (str): The timestamp of the post in the format "dd-mm-yyyy HH:MM:SS".
-                - content (list): A list of sentences extracted from the post's content.
+                - subreddit: The name of the subreddit.
+                - id: The unique identifier of the post.
+                - title: The title of the post.
+                - time: The creation time of the post.
+                - content: The filtered content of the post.
         """
+        # Set the subreddit
+        self.subreddit = self.reddit.subreddit(subreddit)
+
         self.final = []  # Init final list to store data
         # Get top posts from subreddit
         for iter_post in self.subreddit.top(limit=limit):
